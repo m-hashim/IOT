2ac65cb9509104015038033ff61399d72cb1dc7a
Mohammad Hashim <hashnizami@gmail.com>
2019-07-21 07:55:22 +0530

    First Commit

---
+++ src/pt/SimpleMqqtCallback.java
@@ -0,0 +1,27 @@
+package pt;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class SimpleMqqtCallback implements MqttCallback {
+
+	@Override
+	public void connectionLost(Throwable arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("Connection lost to broker");
+	}
+
+	@Override
+	public void deliveryComplete(IMqttDeliveryToken arg0) {
+		
+	}
+
+	@Override
+	public void messageArrived(String arg0, MqttMessage arg1) throws Exception {
+		System.out.println("Message received:\n\t"+new String(arg1.getPayload()) );
+		
+
+	}
+
+}
